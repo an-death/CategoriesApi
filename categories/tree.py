8ed8fc3bcc692cca5ecd8f9ceca2b145cc64158c
as <saenarayoursorrow@gmail.com>
2020-02-15 14:39:40 +0300

    Small refactoring of tree

--- categories/tree.py
+++ categories/tree.py
@@ -7,13 +7,9 @@ from categories.models import Category
 def depth_first_create_category(tree, parent=None) -> Iterable[Category]:
     cat = Category(name=tree['name'], parent=parent)
 
-    if 'children' in tree:
-        create_child = partial(depth_first_create_category, parent=cat)
-        children = map(create_child, tree['children'])
-    else:
-        children = ()
+    create_child = partial(depth_first_create_category, parent=cat)
+    children = map(create_child, tree.get('children', ()))
 
     yield cat
     for c in children:
         yield from c
-
